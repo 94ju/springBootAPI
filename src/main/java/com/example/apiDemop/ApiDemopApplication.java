d3bcb73c45380b43a13a2ca295ed76bb496ca2fe
94ju <janithherath94@gmail.com>
2019-06-29 23:48:53 +0530

    Initial commit

---
+++ src/main/java/com/example/apiDemop/ApiDemopApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiDemop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemopApplication.class, args);
+	}
+
+}
